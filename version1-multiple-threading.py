5c38f5308ea96680a0042275c117ba9f9f8da4d0
Khanh <khanhmeizu1@gmail.com>
2023-06-03 16:58:01 +0700

    first commit

---
+++ version1-multiple-threading.py
@@ -0,0 +1,81 @@
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from pytube import YouTube
+import threading
+
+def open_browser(url):
+    browser = webdriver.Firefox()
+    browser.get(url)
+    time.sleep(2)
+    html = browser.page_source
+    browser.close()
+    return html
+
+def extract_video_urls(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    video_urls = []
+    for link in soup.find_all('a', id='thumbnail'):
+        if 'href' in link.attrs:
+            video_id = link['href']
+            url = f"https://www.youtube.com{video_id}"
+            video_urls.append(url)
+        else:
+            print("No href attribute found")
+    return video_urls
+
+
+def download_video(url, task_name):
+    try:
+        yt = YouTube(url)
+        print('YouTube title:', yt.title)
+        stream = yt.streams.get_highest_resolution()
+        output_path = task_name
+        stream.download(output_path)
+    except Exception as e:
+        print(f"Error retrieving title for video URL: {url}")
+        print(f"Error message: {str(e)}")
+
+def download_videos(url, task_name):
+    html_source = open_browser(url)
+    video_urls = extract_video_urls(html_source)
+    
+    # Create a list to store the threads
+    threads = []
+    
+    for url in video_urls:
+        # Create a thread for each video download
+        thread_video = threading.Thread(target=download_video, args=(url, task_name))
+        threads.append(thread_video)
+        thread_video.start()
+    
+    # Wait for all threads to complete
+    for thread_video in threads:
+        thread_video.join()
+    
+    print(f"Completed task: {task_name}")
+
+
+# Main code
+if __name__ == "__main__":
+
+    channel_name = "@khanhichi99"
+    video_shorts = f"https://www.youtube.com/{channel_name}/shorts"
+    videos = f"https://www.youtube.com/{channel_name}/videos"
+    video_streams = f"https://www.youtube.com/{channel_name}/streams"
+
+
+    download_videos(video_shorts, "Shorts")
+    download_videos(videos, "videos")
+
+    
+
+    print("All tasks completed.")
+
+
+
+
+
+
+
+
